acb2c9790c8750365eca694f3f0217e67c25839c
Vyacheslav <r_slavik2015@inbox.ru>
2024-07-15 16:56:10 +0300

    ips change

--- lib/features/user_account/data/data_sources/remote/auth_api_service.g.dart
+++ lib/features/user_account/data/data_sources/remote/auth_api_service.g.dart
@@ -13,7 +13,7 @@ class _AuthApiService implements AuthApiService {
     this._dio, {
     this.baseUrl,
   }) {
-    baseUrl ??= 'http://192.168.0.117:2222';
+    baseUrl ??= 'http://92.118.95.126:8090';
   }
 
   final Dio _dio;
